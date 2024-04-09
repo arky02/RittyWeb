23c6b57220aab79c480696cd5c3e3fcdbd506db4
arky02 <kyean07@naver.com>
2024-04-10 02:07:50 +0900

    design: 모바일 디자인 수정

--- src/App.jsx
+++ src/App.jsx
@@ -41,10 +41,9 @@ function App() {
 
   return (
     <main
-      className="w-[395px] h-[100vh] flex flex-col justify-center p-[25px] relative"
+      className="w-[395px] h-[100vh] flex flex-col md:justify-center  md:-mt-[20px] mt-[10px] p-[25px] relative overflow-x-hidden"
       style={{
         backgroundColor: isOpen ? "#FFFFFF" : "#FFFEFA",
-        marginTop: isOpen ? "0" : "-70px",
       }}
     >
       <div className="flex flex-col items-center gap-[15px]">
